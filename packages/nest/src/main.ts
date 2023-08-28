ee2945b281485f9be6e986130232cbc7c9e8ce38
Grégory Heitz <edno@edno.io>
2023-08-28 11:06:38 +0000

    :technologist:  clean up dependencies

--- packages/nest/src/main.ts
+++ packages/nest/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  app.get(AppService);
 }
 bootstrap();
